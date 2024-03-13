a4bdd195009ee4b82fd408ddfec7e6fa5a528dd5
Amin <abdimamin@gmail.com>
2024-03-13 22:59:22 +0000

    chore: add prayer tile comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
 import Image from "next/image";
 
+function PrayerTile() {
+  return (
+    <div className="h-24 px-8 flex gap-2 justify-between items-center rounded-2xl w-full bg-white border border-solid border-gray-300">
+    <div className="flex gap-6 items-center ">
+    <div className="flex items-center">
+      <Image
+    className="rounded-3xl"
+    src="/icon-192x192.png"
+    alt="Next.js Logo"
+    width={50}
+    height={50}
+    priority
+  />
+    </div>
+    <div>prayer name</div>
+    </div>
+    <div className="flex flex-col items-center font-light text-sm">
+      <p>Adhan: 17:00</p>
+      <p>Iqama: 17:00</p>
+    </div>
+  </div>
+  );
+}
+
 export default function Home() {
   return (
     <main className="flex flex-col h-screen items-start bg-gray-200">
@@ -7,11 +31,12 @@ export default function Home() {
         <p className="text-white text-3xl">Welcome</p>
       </div>
       <div className="h-3/5 p-6 w-full rounded-b-3xl flex flex-col gap-4">
-        <div className="h-24 rounded-2xl w-full bg-white border border-solid border-gray-300"></div>
-        <div className="h-24 rounded-2xl w-full bg-white border border-solid border-gray-300"></div>
-        <div className="h-24 rounded-2xl w-full bg-white border border-solid border-gray-300"></div>
-        <div className="h-24 rounded-2xl w-full bg-white border border-solid border-gray-300"></div>
-        <div className="h-24 rounded-2xl w-full bg-white border border-solid border-gray-300"></div>
+        
+        <PrayerTile />
+        <PrayerTile />
+        <PrayerTile />
+        <PrayerTile />
+        <PrayerTile />
       </div>
     </main>
   );
