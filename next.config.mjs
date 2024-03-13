387cf2b8d8185a4081a6e9da03dc268e74d9c733
Amin <abdimamin@gmail.com>
2024-03-13 22:01:55 +0000

    chore: add initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+    dest: "public",
+    // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+    register: true, // Register the PWA service worker
+    skipWaiting: true, 
+});
+
+const nextConfig = {
+    reactStrictMode: true, // Enable React strict mode for improved error handling
+    swcMinify: true      // Enable SWC minification for improved performance
+    // compiler: {
+    //   removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    // },
+};
+
+export default withPWA(nextConfig);
+
+// export default nextConfig;
