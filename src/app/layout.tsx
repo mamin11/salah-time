0810d77871712b58c45df32b045dcea565351e0f
Amin <abdimamin@gmail.com>
2024-03-13 22:41:10 +0000

    chore: add base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 // themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
 // viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
 export const metadata: Metadata = {
-  title: "Salah Times",
-  description: "Prayer times app",
+  title: "Assunah",
+  description: "Assunah Islamic Center",
   generator: "Next.js",
   manifest: "/manifest.json",
   keywords: ["salah", "prayer", "salah-time", "prayer-time"],
