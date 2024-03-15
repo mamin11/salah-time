28a43d0b1db6e394a3ce9230d365a8d89f503f27
Amin <abdimamin@gmail.com>
2024-03-15 04:44:24 +0000

    chore: pass props to prayer tile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
-function PrayerTile() {
+interface Prayer {name: string, adhan: string, iqama: string}
+
+function PrayerTile({prayer} :{prayer: Prayer}) {
   return (
     <div className="h-20 pl-4 pr-8 flex gap-2 justify-between items-center rounded-2xl w-full bg-white border border-solid border-gray-300">
     <div className="flex gap-6 items-center ">
@@ -14,15 +16,23 @@ function PrayerTile() {
     priority
   />
     </div>
-    <div>prayer name</div>
+    <div>{prayer.name}</div>
     </div>
     <div className="flex flex-col items-center font-light text-sm">
-      <p>Adhan: 17:00</p>
-      <p>Iqama: 17:00</p>
+      <p>Adhan: {prayer.adhan}</p>
+      <p>Iqama: {prayer.iqama}</p>
     </div>
   </div>
   );
 }
+
+const prayers :Prayer[] = [
+  {name: "Fajr", adhan: "04:34", iqama: "04:54"},
+  {name: "Dhuhr", adhan: "12:30", iqama: "12:45"},
+  {name: "Asr", adhan: "03:44", iqama: "04:02"},
+  {name: "Maghrib", adhan: "18:04", iqama: "18:02"},
+  {name: "Isha", adhan: "19:30", iqama: "19:45"}
+];
 
 export default function Home() {
   return (
@@ -32,11 +42,9 @@ export default function Home() {
       </div>
       <div className="h-3/4 p-6 w-full rounded-b-3xl flex flex-col gap-4">
         
-        <PrayerTile />
-        <PrayerTile />
-        <PrayerTile />
-        <PrayerTile />
-        <PrayerTile />
+        {prayers.map((prayer) => (
+          <PrayerTile prayer={prayer} />
+        ))}
       </div>
     </main>
   );
